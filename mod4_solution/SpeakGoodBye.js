b32940691ba491a67796e58456fff50f581a8dee
Kirsty Baffour <kbaffour@smartadserver.com>
2022-05-30 10:32:58 +0200

    My page.

--- mod4_solution/SpeakGoodBye.js
+++ mod4_solution/SpeakGoodBye.js
@@ -6,8 +6,3 @@ byeSpeaker.sayGoodBye = function () {
 console.log(speakWord + byeSpeaker.name); 
 } 
 name.byeSpeaker = byeSpeaker 
-<<<<<<< HEAD
-})(name); 
-=======
-})(name); 
->>>>>>> e406605b2bdec9961772a673c2d942e0f884d081
